dcf310af2e799bced093a924e074c5f56284ebe3
Felipe Zampa <fzampa@gmail.com>
2023-05-09 21:39:02 +0200

    Moving Swagger config from polluted Controller to clean json file

--- src/main/java/com/example/weather/api/v1/WeatherApi.java
+++ src/main/java/com/example/weather/api/v1/WeatherApi.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,13 +21,6 @@ import com.example.weather.model.v1.SampleSaveTransfer;
 import com.example.weather.model.v1.SampleTransfer;
 import com.example.weather.service.SampleService;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-
 @RestController
 @RequestMapping("/api/v1/weather")
 public class WeatherApi {
@@ -36,10 +28,6 @@ public class WeatherApi {
 	@Autowired
 	private SampleService sampleService;
 
-	@Operation(summary = "Return specific sample based on it's id")
-	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200", description = "Sample with given ID", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = SampleTransfer.class))),
-			@ApiResponse(responseCode = "404", description = "Sample not found", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))})
 	@GetMapping("/{id}")
 	public ResponseEntity<SampleTransfer> getSample(
 			@PathVariable(required = true) String id) {
@@ -49,10 +37,6 @@ public class WeatherApi {
 				: new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
 
-	@Operation(summary = "Return all samples")
-	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200", description = "List of samples", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = SampleTransfer.class)))),
-			@ApiResponse(responseCode = "404", description = "Sample not found", content = @Content(mediaType = "application/json"))})
 	@GetMapping
 	public ResponseEntity<List<SampleTransfer>> getAllSamples() {
 		List<SampleTransfer> responseList = SampleTransformer
@@ -62,10 +46,6 @@ public class WeatherApi {
 				: new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
 
-	@Operation(summary = "Creates a sample")
-	@io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Sample to be created", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = SampleSaveTransfer.class)), required = true)
-	@ApiResponses(value = {
-			@ApiResponse(responseCode = "201", description = "Created", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE))})
 	@PostMapping
 	public ResponseEntity<?> createSample(
 			@RequestBody(required = true) SampleSaveTransfer transfer) {
@@ -76,11 +56,6 @@ public class WeatherApi {
 		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
 
-	@Operation(summary = "Updates a sample")
-	@io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Sample to be updated, with ID", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = SampleTransfer.class)), required = true)
-	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200", description = "Sample update", content = @Content(mediaType = "application/json")),
-			@ApiResponse(responseCode = "404", description = "Sample not found", content = @Content(mediaType = "application/json"))})
 	@PutMapping
 	public ResponseEntity<?> updateSample(
 			@RequestBody(required = true) SampleTransfer transfer) {
@@ -95,10 +70,6 @@ public class WeatherApi {
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
-	@Operation(summary = "Deletes a sample based on it's id")
-	@ApiResponses(value = {
-			@ApiResponse(responseCode = "204", description = "Deleted"),
-			@ApiResponse(responseCode = "404", description = "Not found")})
 	@DeleteMapping
 	public ResponseEntity<?> deleteSample(
 			@PathVariable(required = true) String id) {
--- src/main/java/com/example/weather/controller/MainController.java
+++ src/main/java/com/example/weather/controller/MainController.java
@@ -2,22 +2,14 @@ package com.example.weather.controller;
 
 import java.util.Optional;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 @RestController
 @RequestMapping("/api")
 public class MainController {
 
-	@GetMapping(value = { "/ping/{message}", "/ping" })
-	@Operation(summary = "Ping. Returns pong.")
-	@ApiResponse(responseCode = "200", description = "Expected pong response", content = @Content)
 	public String ping(@PathVariable(required = false) String message) {
 		return "pong " + Optional.ofNullable(message).orElse("");
 	}
